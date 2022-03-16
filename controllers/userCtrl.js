cb3e4998feb9e4b1b637b14921fd53da6676e917
TUVX <tuhaha900@gmail.com>
2022-03-16 13:20:02 +0700

    login-logout back-end

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -71,6 +71,51 @@ const userCtrl = {
         }
         
     },
+    login: async (req, res) =>{
+        try {
+            const {email, password} = req.body;
+
+            const user = await Users.findOne({email})
+            if(!user) return res.status(400).json({msg: "user is not existing"})
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "password is not correct"})
+
+            // if logging succeed => create access token and refresh token
+            const accesstoken = createAccessToken({id: user._id})
+            const refreshtoken = createRefreshToken({id: user._id})
+
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token',
+                // maxAge: 7*24*60*60*1000 // 7d
+            })
+
+            res.json({accesstoken,msg:"succeed"})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    logout: async (req, res) =>{
+        try {
+            res.clearCookie('refreshtoken', {path: '/user/refresh_token'})
+            return res.json({msg: "Logged out"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    getUser: async (req, res) =>{
+        try {
+            const user = await Users.findById(req.user.id).select('-password')
+            if(!user) 
+                return res.status(400).json({msg: "user ism't existing"})
+
+            res.json(user)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
 }
 
 
